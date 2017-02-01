b9ab8770617d2b3f042a96ffd59cb240528d3986
greicens <gsilva@mail.ccsf.edu>
2017-02-01 11:24:39 -0800

    adds city model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'welcome#index'
-  
+
   get '/users', to: 'users#index'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 
+  get '/cities', to: 'cities#index'
+  get '/cities/new', to: 'cities#new'
+  post '/cities', to: 'cities#create'
+  get '/cities/:id', to: 'cities#show'
+  
 end
---
+++ db/migrate/20170201192034_create_cities.rb
@@ -0,0 +1,11 @@
+class CreateCities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cities do |t|
+      t.string :name
+      t.reference :post
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
