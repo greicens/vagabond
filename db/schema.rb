70403ab9de7b48dee6cd121a02a000e4aa5c0e23
nzoLogic <nzoLogic@gmail.com>
2017-02-01 14:36:31 -0800

    modified user migration to include email, password, rank, and points

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201011114) do
+ActiveRecord::Schema.define(version: 20170201222924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20170201011114) do
     t.string   "current_city"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "rank"
+    t.integer  "points"
+    t.string   "email"
+    t.string   "password"
   end
 
 end
