1afe7c35f0be1e213c273e1305ebc1a73a7b6b11
ryanthomas92 <rthoma11@villanova.edu>
2017-01-31 17:33:39 -0800

    created crud functionality for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    user = User.find_by_id(params[:id])
+    user.update(user_params)
+    redirect_to user_path
+  end
+
+  def destroy
+    user = User.find_by_id(params[:id])
+    user.destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :current_city)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
+  get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/edit', to: 'users#edit'
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
