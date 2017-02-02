07dee4c7939f3cd07b79555abeabf632c2b513ad
greicens <gsilva@mail.ccsf.edu>
2017-02-01 16:24:29 -0800

    adds password_digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170201192034) do
+ActiveRecord::Schema.define(version: 20170201225232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +28,12 @@ ActiveRecord::Schema.define(version: 20170201192034) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "current_city"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "rank"
     t.integer  "points"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
