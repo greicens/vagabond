a4a8785a5e4f8ec752ce3ee583073fad0ffa5974
greicens <gsilva@mail.ccsf.edu>
2017-02-02 22:10:21 -0800

    adds a new user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170202233300) do
 
   # These are extensions that must be enabled in order to support this database
@@ -60,9 +59,9 @@ ActiveRecord::Schema.define(version: 20170202233300) do
     t.integer  "points"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "photo"
     t.string   "slug"
     t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
-    t.string   "photo"
   end
 
   add_foreign_key "posts", "cities"
