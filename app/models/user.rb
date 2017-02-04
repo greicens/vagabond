d221fde734fd1f2c4982b625ae5aaf18d47f9be3
nzoLogic <nzoLogic@gmail.com>
2017-02-04 14:21:44 -0800

    changed users default score value

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   has_many :posts
-
+  after_initialize :init
   mount_uploader :photo, PhotoUploader
   has_secure_password
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
+  end
+
+  def init
+    self.points ||= 15
+    
   end
 
   extend FriendlyId
